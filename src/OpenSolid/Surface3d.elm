0ee380ab108238088f2b662bbe99954593c4a6a1
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2017-08-08 19:55:36 -0400

    Add Surface3d.toMesh

--- src/OpenSolid/Surface3d.elm
+++ src/OpenSolid/Surface3d.elm
@@ -1,9 +1,11 @@
 module OpenSolid.Surface3d
     exposing
         ( pointOn
+        , toMesh
         )
 
 import OpenSolid.Geometry.Types exposing (..)
+import OpenSolid.Mesh exposing (Mesh)
 import OpenSolid.Parametric.Implementation as Implementation
 import OpenSolid.Parametric.Types exposing (..)
 
@@ -11,3 +13,8 @@ import OpenSolid.Parametric.Types exposing (..)
 pointOn : Surface3d -> Point2d -> Point3d
 pointOn =
     Implementation.surface3dPointOn
+
+
+toMesh : Float -> Surface3d -> Mesh ( Point3d, Vector3d )
+toMesh =
+    Implementation.surface3dToMesh
